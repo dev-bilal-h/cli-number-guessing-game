c9bc701d79fa72134e49388508b2e57911c48657
BILALHASSAN23 <siddiquibilal882@gmail.com>
2024-06-26 10:36:13 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green("       WELCOME TO B23 NUMBER GUESSING GAME       "));
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:chalk.magentaBright("Please guess a number between 1-10:"),
+    },
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+    console.log(chalk.blueBright("Congratulation! you gussed right number"));
+} else {
+    console.log(chalk.red("you guessed wrong number"));
+}
